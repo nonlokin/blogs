70331fe00deabe1e9a6729abf5b8e13f1b89b0bf
nonlokin <60163623+nonlokin@users.noreply.github.com>
2020-10-12 17:58:22 +0900

    Add files via upload

---
+++ auth.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+	<h1 class="text-center">authorization</h1>
+	<div class="mx-auto col-4">
+	<?php
+		$correct = "12345";
+	?>
+		<form action="index.php" method="GET">
+			<p>Login</p>
+			<input type="" name="login" class="form-control">
+			<p>Password</p>
+			<input type="" name="password" class="form-control">
+			<button class="bg-primary btn text-white mt-2">Submit</button>						
+		</form>
+		<?php
+		?>
+	</div>
+</body>
+</html>
---
+++ fadmin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+	<div class="" style="margin-left:500px;">
+		<form action="index.php" method="GET">
+			<h1 class="mx-auto">Заголовок</h1>
+			<input type="" name="title" class="form-control col-6">
+            <h1 class="mx-auto">Текст поста</h1>
+			<input type="" name="short_text" class="form-control col-6">
+            <h1 class="mx-auto">Автор</h1>
+			<input type="" name="users" class="form-control col-6">
+            <h1 class="mx-auto">Картинка</h1>
+			<input type="" name="img" class="form-control col-6">
+			<button class="btn bg-primary">submit</button>													
+		</form>
+	</div>
+</body>
+</html>
